3d2f8053103b2f2ca555db3ca19b031f17f2a45a
MCPutro <em.chepe@gmail.com>
2022-03-10 10:16:45 +0700

    add form int html

---
+++ modul9-Web/form2_test.go
@@ -0,0 +1,47 @@
+package modul9_Web
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"testing"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Fprintf(w, "Hello world")
+}
+
+func me(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Fprintf(w, "Mehhhhh?")
+}
+
+func input(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("method:", r.Method)
+
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("templates/input.gohtml")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		fmt.Println("Your Name:", r.Form["name"])
+		fmt.Println("Your Address:", r.Form["address"])
+		fmt.Fprintf(w, "Your name is %s and you are located in %s", r.Form["name"], r.Form["address"])
+	}
+}
+
+func TestMainServer(t *testing.T) {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/me", me)
+	http.HandleFunc("/input", input)
+	err := http.ListenAndServe(":6969", nil)
+
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
